07e0526141c4077670671690f248883e59d5b81d
Shahaf221b <81834995+Shahaf221b@users.noreply.github.com>
2021-04-03 02:29:37 +0300

    tst

--- src/test/GeneralCheckingFunctions.java
+++ src/test/GeneralCheckingFunctions.java
@@ -11,6 +11,10 @@ public class GeneralCheckingFunctions {
     public static String getGithubLink(){
         //change the <username> in the link to the username of the student who
        // created the github project:
+
+
+
+        
         String githubLink = "https://github.com/shahaf221b/ATP-Project";
         return githubLink;
     }
